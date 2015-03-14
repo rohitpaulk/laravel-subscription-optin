86157ba7062bc33212963b189e34fc4872bc789f
Rohit Paul Kuruvilla <rohitpaulk@live.com>
2015-03-15 02:05:40 +0530

    Bootstrap styling

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,8 +1,22 @@
 <html>
 	<head>
 		<title>Subscriptions</title>
+		<link rel="stylesheet" href="{{ asset('stylesheets/bootstrap.min.css') }}">
+		<link rel="stylesheet" href="{{ asset('stylesheets/flat-ui.min.css') }}">
+		<link rel="stylesheet" href="{{ asset('stylesheets/main.css') }}">
+		<script src="{{ asset('javascripts/vendor/jquery.min.js') }}"></script>
+		<script src="{{ asset('javascripts/flat-ui.min.js') }}"></script>
+		<script src="{{ asset('javascripts/validator.min.js') }}"></script>
 	</head>
 	<body>
-		@yield('content')
+		<nav class="navbar navbar-inverse navbar-fixed-top">
+			<div class="container">
+				 <a class="navbar-brand" href="#">Awesome Newsletter</a>
+			</div>
+		</nav>
+
+		<div class="container">
+			@yield('content')
+		</div>
 	</body>
 </html>
--- resources/views/subscribers/create.blade.php
+++ resources/views/subscribers/create.blade.php
@@ -1,29 +1,47 @@
 @extends('layouts.base')
 
 @section('content')
-	<h1>New Subscription</h1>
+	<div class="row">
+		<div class="page-header text-center">
+			<h3>Subscribe Below</h3>
+			<p>Enter your details to get started</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+			@if ($errors->any())
+				<ul>
+				@foreach ($errors->all() as $error)
+					<li>{{ $error }}</li>
+				@endforeach
+				</ul>
+			@endif
 
-	@if ($errors->any())
-		<ul>
-		@foreach ($errors->all() as $error)
-			<li>{{ $error }}</li>
-		@endforeach
-		</ul>
-	@endif
+
+			{!! Form::open(['url' => 'subscribers']) !!}
+
+				<div class="form-group">
+					{!! Form::label('first_name', 'First Name:', ["class" => "control-label"]) !!}
+					{!! Form::text('first_name', null, ["class" => "form-control", "placeholder" => "Sherlock"]) !!}
+				</div>
+
+				<div class="form-group">
+					{!! Form::label('last_name', 'Last Name:', ["class" => "control-label"]) !!}
+					{!! Form::text('last_name', null, ["class" => "form-control", "placeholder" => "Holmes"]) !!}
+				</div>
+
+				<div class="form-group">
+					{!! Form::label('email', 'Email:', ["class" => "control-label"]) !!}
+					{!! Form::text('email', null, ["class" => "form-control", "placeholder" => "sherlock@gmail.com"]) !!}
+				</div>
+
+				<div class="form-group">
+					{!! Form::submit('Submit', ["class" => "btn btn-primary btn-block"]) !!}
+				</div>
 
 
-	{!! Form::open(['url' => 'subscribers']) !!}
 
-		{!! Form::label('first_name', 'First Name:') !!}
-		{!! Form::text('first_name') !!}
-
-		{!! Form::label('last_name', 'Last Name:') !!}
-		{!! Form::text('last_name') !!}
-
-		{!! Form::label('email', 'Email:') !!}
-		{!! Form::text('email') !!}
-
-		{!! Form::submit('Submit') !!}
-
-	{!! Form::close() !!}
+			{!! Form::close() !!}
+		</div>
+	</div>
 @stop
