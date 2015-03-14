ce493d26edea465f2eb2d531e059e71a86786d25
Rohit Paul Kuruvilla <rohitpaulk@live.com>
2015-03-15 02:28:13 +0530

    Client side validation

--- resources/views/subscribers/create.blade.php
+++ resources/views/subscribers/create.blade.php
@@ -3,36 +3,36 @@
 @section('content')
 	<div class="row">
 		<div class="page-header text-center">
-			<h3>Subscribe Below</h3>
+			<h3>Subscribe</h3>
 			<p>Enter your details to get started</p>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-			@if ($errors->any())
-				<ul>
-				@foreach ($errors->all() as $error)
-					<li>{{ $error }}</li>
-				@endforeach
-				</ul>
-			@endif
-
-
-			{!! Form::open(['url' => 'subscribers']) !!}
+			{!! Form::open(['url' => 'subscribers', 'data-toggle' => 'validator']) !!}
 
 				<div class="form-group">
 					{!! Form::label('first_name', 'First Name:', ["class" => "control-label"]) !!}
-					{!! Form::text('first_name', null, ["class" => "form-control", "placeholder" => "Sherlock"]) !!}
+					{!! Form::text('first_name', null, ["class" => "form-control", "placeholder" => "Sherlock", "required" => "true"]) !!}
+					<div class="help-block with-errors">
+						{{ $errors->first('first_name') }}
+					</div>
 				</div>
 
 				<div class="form-group">
 					{!! Form::label('last_name', 'Last Name:', ["class" => "control-label"]) !!}
-					{!! Form::text('last_name', null, ["class" => "form-control", "placeholder" => "Holmes"]) !!}
+					{!! Form::text('last_name', null, ["class" => "form-control", "placeholder" => "Holmes", "required" => "true"]) !!}
+					<div class="help-block with-errors">
+						{{ $errors->first('last_name') }}
+					</div>
 				</div>
 
 				<div class="form-group">
 					{!! Form::label('email', 'Email:', ["class" => "control-label"]) !!}
-					{!! Form::text('email', null, ["class" => "form-control", "placeholder" => "sherlock@gmail.com"]) !!}
+					{!! Form::email('email', null, ["class" => "form-control", "placeholder" => "sherlock@gmail.com", "required" => "true", "data-error" => "Email address is not valid"]) !!}
+					<div class="help-block with-errors">
+						{{ $errors->first('email') }}
+					</div>
 				</div>
 
 				<div class="form-group">
