3a2280149032ab0b869df98caad3db637dd5c912
Rohit Paul Kuruvilla <rohitpaulk@live.com>
2015-03-15 15:52:38 +0530

    Beautify emails

--- resources/views/emails/already_verified.blade.php
+++ resources/views/emails/already_verified.blade.php
@@ -1,3 +1,10 @@
+<div style="text-align: center; font: normal 14px/21px Arial, sans-serif; color: #333; padding: 30px 20%;">
 Hi {{ $full_name }},
-
-You recently submitted a request to subscribe to Awesome Newsletter. We're sending this to notify you that you're already subscribed!
+<br>
+<br>
+You're <b>already subscribed</b> to Awesome Newsletter!
+<div style="text-align: center; color: #999; padding: 0px; margin-top: 20px;">
+    <div style="font: normal 10px/21px Arial, sans-serif;">
+        Sent by Awesome Newsletter, LLC
+    </div>
+</div>
--- resources/views/emails/verification.blade.php
+++ resources/views/emails/verification.blade.php
@@ -1,1 +1,14 @@
-Hi {{ $full_name }}, visit <a href="{{ $verification_link }}">this link</a> to verify your email.
+<div style="text-align: center; font: normal 14px/21px Arial, sans-serif; color: #333; padding: 30px 20%;">
+Hi {{ $full_name }},
+<br>
+<br>
+Click on the link below to verify your subscription to Awesome Newsletter.
+<br>
+<a href="{{ $verification_link }}" style="color: #fff; text-decoration:none; display:inline-block; padding: 0 15px; background: #396; font: normal 14px/40px Arial, sans-serif; white-space: nowrap; border-radius: 3px; margin-top: 30px;">Verify!</a>
+</div>
+
+<div style="text-align: center; color: #999; padding: 0px;">
+    <div style="font: normal 10px/21px Arial, sans-serif;">
+        Sent by Awesome Newsletter, LLC
+    </div>
+</div>
